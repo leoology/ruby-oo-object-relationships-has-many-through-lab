0ac1507c5581211da210256f324d71de98c23370
Leo <efrenn98@gmail.com>
2020-11-15 10:52:03 -0800

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,12 +4,18 @@ class Artist
       def initialize(name)
           @name=name
           @@all<<self
-          @songs=[]
       end 
       def self.all
           @@all
       end
       def songs
-          Artist.songs
+         Song.all.select {|song| song.artist==self}
       end
+      def new_song(name, genre)
+        Song.new(name, self, genre)
+      end
+
+      def genres
+       Song.all.map{|songs| songs.genre}
+      end 
     end
---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+attr_accessor :name
+
+@@all=[]
+def initialize(name)
+    @name=name
+    @@all<<self
+end
+
+def songs
+    Song.all
+end
+def self.all
+    @@all
+end
+
+def artists
+    Song.all.map{|song|song.artist}
+end
+end 
